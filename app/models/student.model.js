9b2a037593bb3b4bc65e9b716c1f8e9b18cc83b1
elizabeth1098 <esobiya@futuryze.com>
2023-03-27 00:01:23 +0530

    contact_number

--- app/models/student.model.js
+++ app/models/student.model.js
@@ -3,22 +3,27 @@ module.exports = (sequelize, Sequelize) => {
     const Student = sequelize.define('student', {
         name: {
             type: Sequelize.STRING,
+            field:'name'
             
         },
         dob: {
             type: Sequelize.DATEONLY,
+            field:'dob'
             
         },
         address: {
             type: Sequelize.STRING,
+            field:'address'
             
         },
         contact_number: {
             type: Sequelize.STRING,
+            field:'contact_number'
             
         },
         course: {
             type: Sequelize.STRING,
+            field:'course'
             
         }
     });
